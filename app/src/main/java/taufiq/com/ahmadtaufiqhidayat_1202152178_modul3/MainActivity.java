75092e2d0e80b14d459361b6c0c113e1c16c350f
Ahmad <ataufiq665@gmail.com>
2018-02-24 23:29:19 +0700

    First Commit

---
+++ app/src/main/java/taufiq/com/ahmadtaufiqhidayat_1202152178_modul3/MainActivity.java
@@ -0,0 +1,13 @@
+package taufiq.com.ahmadtaufiqhidayat_1202152178_modul3;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
